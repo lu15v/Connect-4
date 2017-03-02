99d75a9b17e97afccf57d5e31665e61ac5525099
lu15v <lu15bytes@gmail.com>
2017-03-02 00:34:05 -0600

    ADDED: terminal matrix

--- main.lua
+++ main.lua
@@ -2,17 +2,23 @@
 require("redDisc")
 require("orangeDisc")
 
-local width = 1000
-local heigth = 880
-
-local column_size = width/11
+local width = 650
+local heigth = 560
+local columns_n = 7
+local column_size = width/columns_n
 columns = {}
-x_possitions = {}
 is_red_turn = false
 is_orange_turn = false
 
+local red_symbol = "O"
+local orange_symbol = "X"
+--Save the positions of the discs
 discs = {}
 
+--matrix of the discs
+md = {}
+
+mc = {}
 
 function love.load()
   love.window.setMode(width, heigth, {resizable=true, vsync=false, minwidth=400, minheight=300})
@@ -22,7 +28,7 @@ function love.load()
 
   size = 0
   --array that saves the limits for every column
-  for i = 0, 11 do
+  for i = 0, columns_n do
     columns[i] = size
     size = size + column_size
   end
@@ -34,15 +40,26 @@ function love.load()
   --for selecting what color begins with the game
   math.randomseed(os.time())
   turn = math.random(1, 100)
-
-
   if turn > 50 then
     is_red_turn = true
   else
     is_orange_turn = true
   end
-end
 
+  --creating the matrix
+  for i=1,columns_n do
+    md[i] = {}
+    for j=1,columns_n do
+      md[i][j] = 0
+    end
+  end
+
+  --counters for elements possitions
+
+  for i = 1, columns_n do
+    mc[i] = 7
+  end
+end
 
 
 function love.update(dt)
@@ -51,57 +68,62 @@ end
 
 
 function love.draw()
-  --love.graphics.draw(Reddiscs_controller.disc, 15,5,0)
-  --love.graphics.draw(Orangediscs_controller.disc, 105,5,0)
-  --love.graphics.draw(Reddiscs_controller.disc, 195,5,0)
-  --love.graphics.draw(Orangediscs_controller.disc, 285,5,0)
-  --love.graphics.print("Text", printx, printy)
 
---  for i, v in ipairs(x_possitions) do
---    love.graphics.draw(Reddiscs_controller.disc,v -74, 5, 0)
---    love.graphics.draw(Orangediscs_controller.disc,v -74, 5, 0)
---  end
+--draw the discs
   for i = 1, table.getn(discs), 3 do
     love.graphics.draw(discs[i], discs[i + 1], discs[i + 2], 0)
   end
 
-  ---for background image
-  for i = 0, love.graphics.getWidth() / background:getWidth() do
-    for j = 0, love.graphics.getHeight() / background:getHeight() do
-        love.graphics.draw(background, i * background:getWidth(), j * background:getHeight())
-    end
-  end
+  draw_background(background)
 end
-
-
 
 function love.mousepressed(x, y, button, istouch)
   if button == 1 then
+    separation = {77, 81, 85, 88, 91, 94, 97}
     for i, v in ipairs(columns) do
       if x < v then
-        table.insert(x_possitions, math.floor(v))
         if is_red_turn then
-        --  table.insert(Reddiscs_controller.discs, Reddisc)
           table.insert(discs, Reddiscs_controller.disc)
-          table.insert(discs, v- 74)
-          table.insert(discs, 5)
           is_red_turn = false
           is_orange_turn = true
+          md[mc[i]][i] = red_symbol
         else
-        --  table.insert(Orangediscs_controller.discs, Orangedisc)
           table.insert(discs, Orangediscs_controller.disc)
-          table.insert(discs, v- 74)
-          table.insert(discs, 5)
           is_orange_turn = false
           is_red_turn = true
+          md[mc[i]][i] = orange_symbol
         end
+        table.insert(discs, v - separation[i])
+        table.insert(discs, 5)
+        mc[i] = mc[i] - 1
+        printMatrix()
         break
       end
     end
   end
 end
 
+function printMatrix()
+  for i=1,columns_n do
+    printf("\n")
+    for j=1,columns_n do
+      printf("%s",md[i][j])
+    end
+  end
+  printf("\n-------\n")
+end
+
 
 printf = function(s,...)
            return io.write(s:format(...))
          end -- function
+
+
+function draw_background(background)
+ ---for background image
+ for i = 0, love.graphics.getWidth() / background:getWidth() do
+   for j = 0, love.graphics.getHeight() / background:getHeight() do
+       love.graphics.draw(background, i * background:getWidth(), j * background:getHeight())
+   end
+ end
+end
